1bdf8209175dfee4080bec6b273aee46aeca47ca
Kalamar136 <karlbridi10@gmail.com>
2022-01-22 14:53:01 -0500

    commit

--- Back-end/control.js
+++ Back-end/control.js
@@ -1,1 +1,9 @@
 document.getElementById("haha").innerHTML = "Hello JavaScript";
+var numMovies;
+function chooseNum(num){
+    numMovies = num;
+}
+
+function showNum(){
+    alert("You chose " + numMovies + " movies.");
+}
