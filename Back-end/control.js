4df1184721a0eb2d78a81381e16e590f251c7935
rarchambault <roxanne.archambault@mail.mcgill.ca>
2022-01-22 18:34:11 -0500

    commit

--- Back-end/control.js
+++ Back-end/control.js
@@ -2,3 +2,8 @@ function het(){
 var o = document.querySelector('input[name="num"]:checked').value;
 alert(o);
 }
+
+function getGenre(){
+    var genre = document.querySelector('input[name="genre"]:checked').value;
+    alert(genre);
+}
