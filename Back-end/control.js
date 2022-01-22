ce2beec2401f1ecbbfd5c4de0b715f96ead46623
rarchambault <roxanne.archambault@mail.mcgill.ca>
2022-01-22 18:08:25 -0500

    commit

--- Back-end/control.js
+++ Back-end/control.js
@@ -1,20 +1,8 @@
 
 var document = "./welcome page.html";
 
-function checkBoxCheck() {
-    // Get the checkbox
-    var checkBox1 = document.getElementById("1");
-    var checkBox2 = document.getElementById("2");
-    var checkBox3 = document.getElementById("3");
-    var numberOfMovies;
-    // If the checkbox is checked, change numberOfMovies
-    if (checkBox1.checked == true){
-      numberOfMovies=1;
-    } else if (checkBox2.checked == true){
-        numberOfMovies=2;
-    } else if (checkBox3.checked == true){
-        numberOfMovies=3;
-    }
+function getValue(radio) {
+    var numberOfMovies = radio.value;
   }
 
   if(document.getElementById('next').clicked==true){
