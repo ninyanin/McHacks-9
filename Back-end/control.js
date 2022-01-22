2ae054a91ea36c59862c82a7d51904788d5f01e0
ninyanin <yanin.niramai@mail.mcgill.ca>
2022-01-22 18:40:39 -0500

    commit

--- Back-end/control.js
+++ Back-end/control.js
@@ -1,1 +1,12 @@
-alert("helooo");
+var numMovie;
+var keywords = [];
+var dates = [];
+
+function numMov(){
+numMovie = document.querySelector('input[name="num"]:checked').value;
+console.log(numMovie);
+}
+
+function getDate(){
+
+}
