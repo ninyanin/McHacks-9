bebf00dfa66d74dd950c264c6855e2a7836eb3f5
Kalamar136 <karlbridi10@gmail.com>
2022-01-22 17:00:13 -0500

    commit

--- Back-end/control.js
+++ Back-end/control.js
@@ -1,17 +1,3 @@
-let numMovies;
-let keyworks = [];
-
-const radioButtons = document.querySelectorAll('input[name="num"]');
-
-function getNum() {
-    if(document.getElementById("1").checked){
-        numMovies = 1;
-    }
-    if(document.getElementById("2").checked){
-        numMovies = 2;
-    }
-    if(document.getElementById("4").checked){
-        numMovies = 3;
-    }
-    alert("there are " + numMovies);
- }​
+if (document.getElementById("1").checked == true){
+    alert("hsha");
+}
