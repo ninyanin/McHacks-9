5cf93d7c9576c86b89cbc3fc6dd29e078f552951
rarchambault <roxanne.archambault@mail.mcgill.ca>
2022-01-23 00:10:33 -0500

    commit

--- Back-end/fetch.js
+++ Back-end/fetch.js
@@ -86,15 +86,7 @@ function successFunction(movies){
     
     console.log(title+'\n'+poster+'\n'+genreID+'\n'+overview);
 
-    var img = new Image(); 
-    var div = document.getElementById("suggestion1"); 
-     
-    img.onload = function() { 
-      div.appendChild(img); 
-    }; 
-     
-    img.src = poster;
-
+    document.getElementById("suggestion1").innerHTML.src = poster;
     document.getElementById("title").innerText = title;
     document.getElementById("date").innerText = releaseDate;
     document.getElementById("genre").innerText = genreID;
