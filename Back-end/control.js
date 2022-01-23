bf4efaf28505b695b6d9d7c3f9ee972d8b4f404d
rarchambault <roxanne.archambault@mail.mcgill.ca>
2022-01-22 20:29:09 -0500

    commit

--- Back-end/control.js
+++ Back-end/control.js
@@ -2,6 +2,7 @@ var numMovie;
 var genres = [];
 var checkboxes;
 var dates = [];
+var streaming = [];
 
 function numMov(){
 numMovie = document.querySelector('input[name="num"]:checked').value;
@@ -21,42 +22,55 @@ function getDate() {
     if (document.getElementById("2020").checked) {
         temp=document.getElementById("2020").value;
         dates.push(temp);
-        console.log(dates);
     }
 
     if (document.getElementById("2010").checked) {
         temp=document.getElementById("2010").value;
         dates.push(temp);
-        console.log(dates);
     }
 
     if (document.getElementById("2000").checked) {
         temp=document.getElementById("2000").value;
         dates.push(temp);
-        console.log(dates);
     }
 
     if (document.getElementById("1990").checked) {
         temp=document.getElementById("1990").value;
         dates.push(temp);
-        console.log(dates);
     }
 
     if (document.getElementById("1980").checked) {
         temp=document.getElementById("1980").value;
         dates.push(temp);
-        console.log(dates);
     }
 
     if (document.getElementById("before").checked) {
         temp=document.getElementById("before").value;
         dates.push(temp);
-        console.log(dates);
     }
 
     if (document.getElementById("dc").checked) {
         temp=document.getElementById("dc").value;
         dates.push(temp);
-        console.log(dates);
     }
+
+    console.log(dates);
 }
+
+    function getStream() {
+        if (document.getElementById("theaters").checked) {
+            temp=document.getElementById("theaters").value;
+            streaming.push(temp);
+        }
+    
+        if (document.getElementById("netflix").checked) {
+            temp=document.getElementById("netflix").value;
+            streaming.push(temp);
+        }
+    
+        if (document.getElementById("dvd").checked) {
+            temp=document.getElementById("dvd").value;
+            streaming.push(temp);
+        }
+        console.log(streaming);
+    }
