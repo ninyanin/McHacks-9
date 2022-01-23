233dad43abe6b14c4754c192fee5220b2ddbaed7
rarchambault <roxanne.archambault@mail.mcgill.ca>
2022-01-22 19:05:24 -0500

    commit

--- Back-end/control.js
+++ Back-end/control.js
@@ -17,8 +17,44 @@ function genre() {
 }
 
 function getDate() {
-    if (document.getElementByName("date").checked) {
-        temp.document.getElementByName("name").value;
+    if (document.getElementById("2020").checked) {
+        temp.document.getElementById("2020").value;
+        dates.push(temp);
+        console.log(temp);
+    }
+
+    if (document.getElementById("2010").checked) {
+        temp.document.getElementById("2010").value;
+        dates.push(temp);
+        console.log(temp);
+    }
+
+    if (document.getElementById("2000").checked) {
+        temp.document.getElementById("2000").value;
+        dates.push(temp);
+        console.log(temp);
+    }
+
+    if (document.getElementById("1990").checked) {
+        temp.document.getElementById("1990").value;
+        dates.push(temp);
+        console.log(temp);
+    }
+
+    if (document.getElementById("1980").checked) {
+        temp.document.getElementById("1980").value;
+        dates.push(temp);
+        console.log(temp);
+    }
+
+    if (document.getElementById("before").checked) {
+        temp.document.getElementById("before").value;
+        dates.push(temp);
+        console.log(temp);
+    }
+
+    if (document.getElementById("dc").checked) {
+        temp.document.getElementById("dc").value;
         dates.push(temp);
         console.log(temp);
     }
