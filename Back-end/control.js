39b7cedeefc1180949c28d6f4511572f5a5d8087
Kalamar136 <karlbridi10@gmail.com>
2022-01-22 21:14:22 -0500

    commit

--- Back-end/control.js
+++ Back-end/control.js
@@ -1,61 +1,62 @@
-var numMovie;
+var numMovie = 0; 
 var genres = [];
 var checkboxes;
 var dates = [];
 
-function numMov(){
-numMovie = document.querySelector('input[name="num"]:checked').value;
-console.log(numMovie);
+function numMov() {
+    localStorage.setItem("numMovie", document.querySelector('input[name="num"]:checked').value);
+    console.log(localStorage.getItem("numMovie"));
 }
 
 function genre() {
     checkboxes = document.getElementsByName('genre');
-    for(var i=0, n=checkboxes.length;i<n;i++){
-        if(checkboxes[i].checked){
+    for (var i = 0, n = checkboxes.length; i < n; i++) {
+        if (checkboxes[i].checked) {
             genres.push(checkboxes[i].value);
         }
     }
+    console.log(localStorage.getItem("numMovie"));
 }
 
 function getDate() {
     if (document.getElementById("2020").checked) {
-        temp=document.getElementById("2020").value;
+        temp = document.getElementById("2020").value;
         dates.push(temp);
         console.log(dates);
     }
 
     if (document.getElementById("2010").checked) {
-        temp=document.getElementById("2010").value;
+        temp = document.getElementById("2010").value;
         dates.push(temp);
         console.log(dates);
     }
 
     if (document.getElementById("2000").checked) {
-        temp=document.getElementById("2000").value;
+        temp = document.getElementById("2000").value;
         dates.push(temp);
         console.log(dates);
     }
 
     if (document.getElementById("1990").checked) {
-        temp=document.getElementById("1990").value;
+        temp = document.getElementById("1990").value;
         dates.push(temp);
         console.log(dates);
     }
 
     if (document.getElementById("1980").checked) {
-        temp=document.getElementById("1980").value;
+        temp = document.getElementById("1980").value;
         dates.push(temp);
         console.log(dates);
     }
 
     if (document.getElementById("before").checked) {
-        temp=document.getElementById("before").value;
+        temp = document.getElementById("before").value;
         dates.push(temp);
         console.log(dates);
     }
 
     if (document.getElementById("dc").checked) {
-        temp=document.getElementById("dc").value;
+        temp = document.getElementById("dc").value;
         dates.push(temp);
         console.log(dates);
     }
