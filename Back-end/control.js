8dc0548dfd30a9dd8f7efa7405fd639a2d83206c
rarchambault <roxanne.archambault@mail.mcgill.ca>
2022-01-22 20:05:11 -0500

    commit

--- Back-end/control.js
+++ Back-end/control.js
@@ -18,44 +18,44 @@ function genre() {
 
 function getDate() {
     if (document.getElementById("2020").checked) {
-        temp.document.getElementById("2020").value;
+        temp=document.getElementById("2020").value;
         dates.push(temp);
-        console.log(temp);
+        console.log(dates);
     }
 
     if (document.getElementById("2010").checked) {
-        temp.document.getElementById("2010").value;
+        temp=document.getElementById("2010").value;
         dates.push(temp);
-        console.log(temp);
+        console.log(dates);
     }
 
     if (document.getElementById("2000").checked) {
-        temp.document.getElementById("2000").value;
+        temp=document.getElementById("2000").value;
         dates.push(temp);
-        console.log(temp);
+        console.log(dates);
     }
 
     if (document.getElementById("1990").checked) {
-        temp.document.getElementById("1990").value;
+        temp=document.getElementById("1990").value;
         dates.push(temp);
-        console.log(temp);
+        console.log(dates);
     }
 
     if (document.getElementById("1980").checked) {
-        temp.document.getElementById("1980").value;
+        temp=document.getElementById("1980").value;
         dates.push(temp);
-        console.log(temp);
+        console.log(dates);
     }
 
     if (document.getElementById("before").checked) {
-        temp.document.getElementById("before").value;
+        temp=document.getElementById("before").value;
         dates.push(temp);
-        console.log(temp);
+        console.log(dates);
     }
 
     if (document.getElementById("dc").checked) {
-        temp.document.getElementById("dc").value;
+        temp=document.getElementById("dc").value;
         dates.push(temp);
-        console.log(temp);
+        console.log(dates);
     }
 }
